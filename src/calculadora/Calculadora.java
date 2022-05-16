65609e8a5cebf0861978dc792f55bcc14f68a2a9
Carmen Yelmo <carmenraquelyg@gmail.com>
2022-05-16 10:30:53 +0200

    Creada clase calculadora con atributos y constructores

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author carme
+ */
+public class Calculadora {
+    private double numero1;
+    private double numero2;
+    private String marca;
+    private String modelo;
+
+    public Calculadora(double numero1, double numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public Calculadora() {
+
+    }
+
+}
